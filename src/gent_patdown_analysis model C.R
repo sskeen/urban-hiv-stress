27a5c3c1af5b417e2e89ac759d897089f63dea8a
esmith504 <84626091+esmith504@users.noreply.github.com>
2026-07-20 12:47:10 -0500

    Cleaned Model C code

--- src/gent_patdown_analysis model C.R
+++ src/gent_patdown_analysis model C.R
@@ -1,25 +1,30 @@
 library(gtsummary)
 library(tidyverse)
 library(car)
+library(glmmTMB)
 library(readxl)
+library(broom)
+library(dplyr)
+library(purrr)
 
-# Table C Logistic Regressions
+# Table C Logistic Regressions -- PTSD symptomology outcome
 
-noah_gent_cd4 <- read.csv("C:/Users/Ethan/...gent_pat_cd4_final_060225.csv")
-full395 <- read_xlsx("C:/Users/Ethan/...data.xlsx")
-dat <- read.csv("C:/Users/Ethan/...gent_pat_cd4_geo_restr_051525.csv")
+noah_gent_cd4 <- read.csv("C:/Users/Ethan/Box/SJS EAS NOAH_POL_CD4/clean_data/gent_pat_cd4_final_060225.csv")
+full395 <- read_xlsx("C:/Users/Ethan/Box/SJS EAS NOAH_POL_CD4/clean_data/Data.xlsx")
+dat <- read.csv("C:/Users/Ethan/Box/SJS EAS NOAH_POL_CD4/clean_data/gent_pat_cd4_geo_restr_051525.csv")
 
 # View the loadings with a cutoff
 #print(fit$loadings, digits = 2, cutoff = 0.3, sort = TRUE)
 
 
-table(dat$ptsd_score)
-
+# Merging NOAH Baseline with recoded set to get scales: 
 dat_sub <- dat %>% dplyr::select(carc_study_id, ptsd_score)
 full <- left_join(dat_sub, noah_gent_cd4, by = 'carc_study_id')
 
+# Cutpoint of 3 
 full$ptsd <- ifelse(full$ptsd_score>3,1,0)
 
+# Census-tract based rates per 10k for police stops and violent crime rate (911 calls)
 full$pd_rate <- (full$patdown_count/full$pop)*10000
 full$vc_rate <- (full$ViolentCrime_conf_count/full$pop)*10000
 
@@ -29,108 +34,90 @@ full$vc_rate <- (full$ViolentCrime_conf_count/full$pop)*10000
 sub <- full %>% dplyr::select(ML1, ML2,ulss1:ulss21)
 
 
-### Model C
-
-full_blk <- full %>% filter(race___4 == 1)
-
-logistic_model_adj <- glm(
-  ptsd ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + as.factor(race___4) +  
-    pd_call_010m + 
-    vc_call_010m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full_blk
-)
-
-logistic_model_adj %>%
-  tbl_regression(exponentiate = TRUE)
+### Model C - crude 
 
 
+
+crude_model <- function(pred){
+  form <- as.formula(paste("ptsd ~", pred))
   
-  ##
-  
-  logistic_model <- glm(
-    ptsd ~ current_neighborhood_yrs +
-      ML1 +  
-      ML2 + as.factor(race___4) +
-      pd_call_010m + 
-      vc_call_010m +
-      age_current + 
-      gender +
-      as.factor(gen_cat18), 
+  glm(
+    form,
     family = binomial(link = "logit"),
     data = full
   )
+}
 
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
+crude_model("current_neighborhood_yrs")
 
+crude_pred <- c("current_neighborhood_yrs", "as.factor(age60)", "ML1", "ML2",
+                "race___4", "gender", "as.factor(gen_cat18)",
+                "pd_call_010m", "pd_call_025m", "pd_call_050m", "pd_rate", "vc_rate",
+                "vc_call_010m", "vc_call_025m", "vc_call_050m", "ice_race",
+                "ice_race_inc", "sdi")
 
-logistic_model <- glm(
-  ptsd ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + race___4 + 
-    pd_call_025m +
-    vc_call_025m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
+crude_models <- lapply(crude_pred, crude_model)
+
+crude_results <- map2_dfr(
+  crude_models,
+  crude_pred,
+  ~ tidy(.x, conf.int = TRUE, exponentiate = TRUE) %>%
+    mutate(predictor = .y)
 )
 
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
-
-logistic_model <- glm(
-  ptsd ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + race___4 +  
-    pd_call_050m +
-    vc_call_050m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
-
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
+crude_results
 
 
+crude_table <- crude_results %>%
+  filter(term != "(Intercept)") %>%
+  mutate(
+    OR_CI = sprintf("%.2f (%.2f, %.2f)",
+                    estimate, conf.low, conf.high),
+    p.value = round(p.value, 2)
+  ) %>%
+  select(predictor, term, OR_CI, p.value)
 
-logistic_model <- glm(
-  ptsd ~  
-    pd_call_050m +
-    vc_call_050m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
+crude_table
 
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
+#### Adjusted Model  - Individual 
 
-
-
-############
-
-#Alt models 
 
 logistic_model_adj <- glm(
-  bi_dep ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + as.factor(race___4) +  
-    pd_rate +
-    vc_rate +
+  ptsd ~ ML1 + ML2 + pd_call_010m + vc_call_010m + as.factor(age60) + as.factor(gender) + as.factor(race___4), 
+  family = binomial(link = "logit"),
+  data = full
+)
+
+logistic_model_adj %>%
+  tbl_regression(exponentiate = TRUE)
+
+vif(logistic_model_adj)
+
+#### Adjusted Model  - Census Tract  
+
+logistic_model_adj <- glm(
+  ptsd ~ pd_rate + vc_rate + ice_race + as.factor(gen_cat18), 
+  family = binomial(link = "logit"),
+  data = full
+)
+
+logistic_model_adj %>%
+  tbl_regression(exponentiate = TRUE)
+
+
+vif(logistic_model_adj)
+summary(logistic_model_adj)
+
+#### Adjusted Model  - Multilevel  
+
+logistic_model_adj <- glm(
+  ptsd ~ ML1 +  
+    ML2  +  
+    ice_race +
+    pd_call_010m + 
+    vc_call_010m +
     age_current + 
+    as.factor(race___4) + 
     gender +
     as.factor(gen_cat18), 
   family = binomial(link = "logit"),
@@ -140,129 +127,11 @@ logistic_model_adj <- glm(
 logistic_model_adj %>%
   tbl_regression(exponentiate = TRUE)
 
-logistic_model <- glm(
-  bi_dep ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + as.factor(race___4) +
-    pd_call_010m + 
-    vc_call_010m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
-
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
-
-
-logistic_model <- glm(
-  bi_dep ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + race___4 + 
-    pd_call_025m +
-    vc_call_025m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
-
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
-
-logistic_model <- glm(
-  bi_dep ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + race___4 +  
-    pd_call_050m +
-    vc_call_050m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
-
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
-
-vif(logistic_model)
-
-
-### CD4 outcome 
-
-table(full$cd4cat)
-
-full$cd4_low <- ifelse(full$cd4cat==1,1,0)
-full$cd4_med <- ifelse(full$cd4cat==3,0,1)
-
-
-logistic_model_adj <- glm(
-  cd4_med ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + as.factor(race___4) +  
-    pd_rate +
-    vc_rate +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
-
 logistic_model_adj %>%
-  tbl_regression(exponentiate = TRUE)
+  tbl_regression(
+    exponentiate = TRUE,
+    estimate_fun = ~style_number(.x, digits = 6)
+  )
 
-logistic_model <- glm(
-  cd4_med ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + as.factor(race___4) +
-    pd_call_010m + 
-    vc_call_010m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
-
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
-
-
-logistic_model <- glm(
-  cd4_med ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + race___4 + 
-    pd_call_025m +
-    vc_call_025m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
-
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
-
-logistic_model <- glm(
-  cd4_med ~ current_neighborhood_yrs +
-    ML1 +  
-    ML2 + race___4 +  
-    pd_call_050m +
-    vc_call_050m +
-    age_current + 
-    gender +
-    as.factor(gen_cat18), 
-  family = binomial(link = "logit"),
-  data = full
-)
-
-logistic_model %>%
-  tbl_regression(exponentiate = TRUE)
-
-vif(logistic_model)
+summary(logistic_model_adj)
+vif(logistic_model_adj)
